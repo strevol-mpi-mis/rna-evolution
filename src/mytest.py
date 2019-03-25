fc9eb7095c32d28b6215efbeb6335b21ff8a4da9
Cyrille Merleau Nono Saha <nonosaha@mb052.local>
2019-03-25 23:04:46 +0100

    Commit on mytest 3

--- src/mytest.py
+++ src/mytest.py
@@ -67,13 +67,7 @@ def novelty_selection(population,size,k) :
     return choices
 
 
-def save_population(population,gen, selection_method) : 
-    
-    root_path = "../Logs/MyTest/"+selection_method
-    try:
-        os.makedirs(root_path)
-    except OSError :
-        print (" Can not initialize the log folder ")
+def save_population(population,gen, root_path) : 
     
     data = []
     for ind in population : 
@@ -116,9 +110,15 @@ def simple_EA(target, number_of_generation, mut_probs, init_pop,k, selection_met
     print (" Starting of evolution ")
     prev_population = numpy.copy(init_pop) #Initialize the population of RNA
     
+    root_path = "../Logs/MyTest2/"+str(log_folder)+"/"+selection_method
+    try:
+        os.makedirs(root_path)
+    except OSError :
+        print (" Can not initialize the log folder ")
+    
     population_size =  len(init_pop)
     n = number_of_generation
-    save_population(prev_population,0,log_folder+selection_method)
+    save_population(prev_population,0,root_path)
     maxfitness = max([ind.fitness for ind in prev_population])
     while (n > 0) and (maxfitness<1)   :
         
@@ -139,7 +139,7 @@ def simple_EA(target, number_of_generation, mut_probs, init_pop,k, selection_met
         
         maxfitness = max([ind.fitness for ind in prev_population])
         n -=1
-        save_population(prev_population, number_of_generation - n, log_folder+selection_method)
+        save_population(prev_population, number_of_generation - n, root_path)
 
     return newgeneration, number_of_generation - n
 
@@ -156,8 +156,8 @@ def run(number_of_generation, mut_probs, k, log_folder) :
         (strc, mfe) = RNA.fold(seq)
         init_pop.append(Individual.Individual(seq, strc, mfe, fitness(strc, target)))
 
-    ppservers = ()
-    job_server = pp.Server(4, ppservers=ppservers)
+    #ppservers = ()
+    #job_server = pp.Server(4, ppservers=ppservers)
     methods = ["N", "F", "S", "FREQ"]
 
     for method in methods : 
@@ -174,20 +174,16 @@ def main() :
 
     
     number_of_run  = int(sys.argv[1])
-    job_server = pp.Server(0, ppservers=ppservers)
+    job_server = pp.Server(20, ppservers=ppservers)
     
     print "Start running jog", number_of_run
     
     jobs = [(task , job_server.submit(run, (number_of_generation, mut_prob, k, task,), ( fitness_proportion_selection,mutateAll, mutateOne, frequency_proportion_selection, stochastic_selection, save_population,novelty, novelty_selection, fitness, hamming_distance, simple_EA),
-                                            ("numpy", "Individual", "RNA", "random","Logger","pandas","os","Initializer", "Landscape", "pp"))) for task in range(number_of_run)]
+                                            ("numpy", "Individual", "RNA", "random","Logger","pandas","os","Initializer", "Landscape", "collections"))) for task in range(number_of_run)]
     
     for task, job in jobs : 
         job()
-    """
-    for i in range(number_of_run) : 
-        run(number_of_generation,mut_prob, k ,  str(i)+"/")
-    print "End of jobs"
-    """
+
 
 if __name__ == "__main__":
     main()
